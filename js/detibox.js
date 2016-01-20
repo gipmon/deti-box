6fbc746193ca04bf227fba94a8058bae3ede2cc4
Rafael Ferreira <mail@rafaelferreira.pt>
2016-01-20 12:39:21 +0000

    done, next content

--- js/detibox.js
+++ js/detibox.js
@@ -11,21 +11,59 @@ $(document).ready(function() {
     }
   });
 
-  for (var i = 0; i < course.years.length; i++) {
-    array[i]
-  }
-  <div class="col-md-4 col-sm-6">
-       <div class="panel panel-default">
-          <div class="panel-heading"><h4>Programação II</h4></div>
-          <div class="panel-body">
-            <ul class="list-group">
-            <li class="list-group-item">Modals</li>
-            <li class="list-group-item">Sliders / Carousel</li>
-            <li class="list-group-item">Thumbnails</li>
-            </ul>
-          </div>
-       </div>
-  </div>
+  var ative = true;
 
-  console.log( course );
+  var yearAtive = course.years[0].id;
+  setSecreen(yearAtive);
+
+  for (var i = 0; i < course.years.length; i++){
+    if(ative){
+      $("#yearsNavBar").append("<li class=\"active\"><a href=\"#"+course.years[i].id+"\">"+course.years[i].year+"</a></li>")
+      ative = false;
+    }else{
+      $("#yearsNavBar").append("<li><a href=\"#"+course.years[i].id+"\">"+course.years[i].year+"</a></li>");
+    }
+  }
+
+  $('li > a').click(function() {
+    $("li.active").removeClass("active");
+    $(this).parent().addClass('active');
+    var ative = $(this).attr("href").replace("#", "");
+    setSecreen(ative);
+  });
+
+  function setSecreen(idAtive){
+    for (var i = 0; i < course.years.length; i++) {
+      if(course.years[i].id==idAtive){
+        $("#displayHtml").slideUp()
+        $("#displayHtml").html("");
+        var htmlTo = "";
+        for(var j = 0; j < course.years[i].subjects.length; j++){
+            htmlTo += "<div class=\"col-md-4 col-sm-6\"><div class=\"panel panel-default\">";
+            htmlTo += "<div class=\"panel-heading\"><h4>" + course.years[i].subjects[j].name + "</h4></div>";
+            htmlTo += "<div class=\"panel-body\"><div class=\"list-group\">";
+
+            for(var k=0; k < course.years[i].subjects[j].links.length; k++){
+              htmlTo += "<a href=\"#showLinkModal\" rel=\"course\" url=\"" + course.years[i].subjects[j].links[k].url + "\" role=\"button\" data-toggle=\"modal\" class=\"list-group-item\">" + course.years[i].subjects[j].links[k].name + "</a>";
+            }
+
+            htmlTo += "</div></div></div></div>";
+        }
+
+        $("#displayHtml").append(htmlTo);
+        $("#displayHtml").slideDown();
+        listener();
+        break;
+      }
+    }
+  }
+
+  function listener(){
+    $("a[rel='course']").click(function(){
+      $("#linkName").text($(this).text());
+      $("#hrefLink").text($(this).attr("url").substring(0,26));
+      $("#hrefLink").attr("href", $(this).attr("url"));
+      console.log($(this));
+    });
+  }
 });
